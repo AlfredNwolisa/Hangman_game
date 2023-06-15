5aa7da6694740fe919f528981f11695d08f1357d
Alfie_N <olisafredo@gmail.com>
2023-06-16 00:22:14 +0100

    code upgrade

--- CS50/hangman.py
+++ CS50/hangman.py
@@ -1,10 +1,67 @@
-#Step 3
-
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+lives = 6
 #Testing code
 #print(f'Pssst, the solution is {chosen_word}.')
 
@@ -13,7 +70,7 @@ display = []
 for _ in range(word_length):
     display += "_"
 
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+#loop to stop and continue event
 while "_" in display:
     guess = input("Guess a letter: ").lower()
 
@@ -23,4 +80,13 @@ while "_" in display:
         if letter == guess:
             display[position] = letter
     print(display)
-print(f"You've won! The word is {chosen_word}.")
+    if guess not in chosen_word:
+        lives -=1
+        if lives == 0:
+            print("you lose")
+            break
+while "_" not in display:
+    print(f"You've won fam! The word is {chosen_word}!.")
+    break
+
+print(stages[lives])
