618b03500b4894f7899523be71dcb556e2a40d54
Alfie_N <olisafredo@gmail.com>
2023-06-19 20:55:42 +0100

    Code upgrade

--- CS50/hangman.py
+++ CS50/hangman.py
@@ -1,12 +1,17 @@
+from distutils.command import clean
 import random
 import hangman_words
 import hangman_art
+import clear
 
 word_list = hangman_words.word_list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+#def clear():
+    #os.system('cls' if os.name == 'nt' else 'clear')
 
 lives = 6
+
 #Testing code
 #print(f'Pssst, the solution is {chosen_word}.')
 
@@ -20,7 +25,10 @@ for _ in range(word_length):
 #loop to stop and continue event
 while "_" in display:
     guess = input("Guess a letter: ").lower()
+    # Clear the screen
+    clear()
     if guess in display:
+        print()
         print(f"you already guessed the letter {guess}")
 
     #Check guessed letter
@@ -30,13 +38,16 @@ while "_" in display:
             display[position] = letter
     print(display)
     if guess not in chosen_word:
+        print()
         print(f"{guess}, is not part of the word")
         lives -=1
         if lives == 0:
-            print("you lose")
+            print()
+            print("You lose. You've been hung! ☠️😵")
             break
     print(hangman_art.stages[lives])
 while "_" not in display:
-    print(f"You've won fam! The word is {chosen_word}!.")
+    print()
+    print(f"You've won fam! The word is {chosen_word}! 🕺⚔️.")
     break
 print(hangman_art.stages[lives])
